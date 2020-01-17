b4bdf971b9e67f03480b541046710c3fa15f34f3
Dongwon Lim <idw111@gmail.com>
2020-01-17 16:28:02 +0900

    fixed to reload texts when new props.text is set

--- src/index.js
+++ src/index.js
@@ -4,53 +4,59 @@ import { disassemble, assemble } from 'hangul-js';
 const DIRECTION_NONE = 0;
 const DIRECTION_RIGHT = 1;
 const DIRECTION_LEFT = -1;
+const defaultState = {
+	text: '',
+	index: 0,
+	cursor: 0,
+	direction: DIRECTION_RIGHT,
+	storedDirection: DIRECTION_RIGHT
+};
 
 /**
- * 
- * @param {Array} texts 
- * @param {number} interval 
- * @param {number} waitingInterval 
+ *
+ * @param {Array} texts
+ * @param {number} interval
+ * @param {number} waitingInterval
  */
 const useHangulTypewriter = (texts = [], interval = 50, waitingInterval = 3000) => {
-    const disassembledTexts = texts.map(text => disassemble(text).join('') + ' ');
-	const [text, setText] = useState('');
-	const [index, setIndex] = useState(0);
-	const [cursor, setCursor] = useState(0);
-	const [direction, setDirection] = useState(DIRECTION_RIGHT);
-	const [storedDirection, setStoredDirection] = useState(DIRECTION_RIGHT);
-	const timerRef = useRef(null)
+	const disassembledTexts = texts.map(text => disassemble(text).join('') + ' ');
+	const [state, setState] = useState(defaultState);
+	const timerRef = useRef(null);
 	useEffect(() => {
-		if (direction !== 0 && texts.length > 0) {
-			const assembledFulltext = texts[index];
-			const disassembledFulltext = disassembledTexts[index];
-			const text = (direction > 0 ? disassembledFulltext : assembledFulltext).substr(0, cursor);
+		setState(defaultState);
+	}, [texts]);
+	useEffect(() => {
+		if (state.direction !== 0 && texts.length > 0) {
+			const assembledFulltext = texts[state.index];
+			const disassembledFulltext = disassembledTexts[state.index];
+			const text = (state.direction > 0 ? disassembledFulltext : assembledFulltext).substr(0, state.cursor);
 			timerRef.current = setTimeout(
 				() => {
-					setText(assemble(text));
-					setCursor(cursor + direction);
-					if (text === disassembledFulltext) {
-						setDirection(DIRECTION_LEFT);
-						setCursor(assembledFulltext.length);
-					}
-					if (direction < 0 && cursor < 0) {
-						setDirection(DIRECTION_RIGHT);
-						setIndex((index + 1) % texts.length);
-					}
+					setState({
+						...state,
+						text: assemble(text),
+						cursor: text === disassembledFulltext ? assembledFulltext.length : state.cursor + state.direction,
+						direction: text === disassembledFulltext ? DIRECTION_LEFT : state.direction < 0 && state.cursor < 0 ? DIRECTION_RIGHT : state.direction,
+						index: state.direction < 0 && state.cursor < 0 ? (state.index + 1) % texts.length : state.index
+					});
 				},
-				cursor >= disassembledFulltext.length ? waitingInterval : interval
+				state.cursor >= disassembledFulltext.length ? waitingInterval : interval
 			);
 			return () => clearTimeout(timerRef.current);
 		}
-	}, [index, cursor, direction]);
-	const pause = () => setStoredDirection(direction) || setDirection(DIRECTION_NONE);
-	const resume = () => setDirection(storedDirection);
-	const toggle = () => (direction !== DIRECTION_NONE ? pause() : resume());
-	const reset = () => {
-		setIndex(0);
-		setCursor(0);
-		setDirection(1);
+	}, [state]);
+	const pause = () => {
+		if (state.direction === DIRECTION_NONE) return;
+		setState({ ...state, storedDirection: state.direction, direction: DIRECTION_NONE });
 	};
-	return [text, toggle, reset, pause, resume];
-}
+	const resume = () => {
+		if (state.direction !== DIRECTION_NONE) return;
+		setState({ ...state, direction: state.storedDirection });
+	};
+	const toggle = () => (state.direction !== DIRECTION_NONE ? pause() : resume());
+	const reset = () => setState(defaultState);
+
+	return [state.text, toggle, reset, pause, resume];
+};
 
 export default useHangulTypewriter;
