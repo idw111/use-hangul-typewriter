1c30e71eac36fec4be0e0ca8f118ee409cdaf81b
Dongwon Lim <idw111@gmail.com>
2020-01-20 16:27:51 +0900

    update example

--- example/index.js
+++ example/index.js
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import useHangulTypewriter from '../src';
 
-const texts1 = ['가나다라마바사', '오늘은 금요일', '치키치키 차카차카 초코초코초'];
-const texts2 = ['아자차카타파하', '오늘은 월요일', '나쁜일을 해라'];
+const texts = [
+	['가나다라마바사', '오늘은 금요일', '치키치키 차카차카 초코초코초'],
+	['아자차카타파하', '오늘은 월요일', '나쁜일을 해라']
+];
 
 const Example = ({}) => {
-	const [texts, setTexts] = useState(texts1);
-	const [text, toggle, reset, pause, resume] = useHangulTypewriter(texts, 50, 1000);
+	const [index, setIndex] = useState(0);
+	const [text, toggle, reset, pause, resume] = useHangulTypewriter(texts[index], 50, 1000);
 	return (
 		<>
 			<div className="typewriter" style={{ fontSize: 24, lineHeight: '40px' }}>
@@ -16,7 +18,7 @@ const Example = ({}) => {
 			</div>
 			<button onClick={pause}>pause</button>
 			<button onClick={resume}>resume</button>
-			<button onClick={() => setTexts(texts2)}>change texts</button>
+			<button onClick={() => setIndex((index + 1) % texts.length)}>change texts</button>
 		</>
 	);
 };
